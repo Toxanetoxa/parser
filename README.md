bd14ccb61295f8cdbf7350120465ee272a1457f5
Anton Sotnik <toxanetoxa@gmail.com>
2024-05-22 13:07:22 +0300

    make docker

---
+++ README.md
@@ -0,0 +1,9 @@
+1. Создать .env фаил: по аналогии с .env.example
+   URL_SITEMAP=https://example.com/sitemap.xml
+   PATH_PARSE=/path/to/parse
+   PATH_FORMAT=/path/to/format
+   PATH_GROUP=/path/to/group
+
+2. Установить docker-compose -> инструкция https://docs.docker.com/compose/install/
+
+3. Поднять образ в докере
--- cmd/parser/main.go
+++ cmd/parser/main.go
@@ -9,18 +9,36 @@ import (
 	"paser/internal/parser/group"
 	"paser/internal/parser/pages"
 	"paser/internal/parser/sitemap"
+	"paser/internal/utils/checkFolder"
 	"paser/internal/utils/mapFolder"
 )
 
 func main() {
-	err := godotenv.Load("../../.env")
+	err := godotenv.Load(".env.prod")
 	if err != nil {
-		log.Fatalf("%s, Ошибка загрузки файла .env")
+		log.Fatalf("Ошибка загрузки файла .env: %v", err)
+		return
 	}
 	url := os.Getenv("URL_SITEMAP")
+
+	var command string
+	fmt.Printf("\nwrite to start parsing: start\n")
+	_, _ = fmt.Scan(&command)
+
+	if command != "start" {
+		fmt.Printf("\nERROR! parsing has not started\n")
+		return
+	}
+
+	pathResult := os.Getenv("PATH_RESULT")
+	checkFolder.Check(pathResult)
 	pathParse := os.Getenv("PATH_PARSE")
+	checkFolder.Check(pathParse)
 	pathFormat := os.Getenv("PATH_FORMAT")
+	checkFolder.Check(pathFormat)
 	pathGroup := os.Getenv("PATH_GROUP")
+	checkFolder.Check(pathGroup)
+
 	nameFile, err := sitemap.New(url, pathParse)
 	if err != nil {
 		return
@@ -28,8 +46,7 @@ func main() {
 	formater.Formating(nameFile, pathParse, pathFormat)
 	group.Group(nameFile, pathFormat, pathGroup)
 	pages.Get(url, nameFile, pathGroup)
-
 	mapFolder.MakeMapComics()
 
-	fmt.Print("---------DONE!!!!---------")
+	fmt.Print("<---------DONE!!!! parsing completed successfully!!! --------->")
 }
